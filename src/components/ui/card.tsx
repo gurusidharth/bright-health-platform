f1e15b61e691fad5e6cc8a366cbb3b9213a4dc7b
GS2911 <gurusidharth@connectoutsolutions.com>
2026-07-08 15:32:41 +0100

    Fix GitHub Pages

--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -50,6 +50,6 @@ const CardFooter = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDiv
     <div ref={ref} className={cn("flex items-center p-6 pt-0", className)} {...props} />
   ),
 );
-CardFooter.displayName = "CardFooter";
+CardFooter.displayName = "CardFooter"; 
 
 export { Card, CardHeader, CardFooter, CardTitle, CardDescription, CardContent };
